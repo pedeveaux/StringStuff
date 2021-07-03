e3bb3232a40942b1c66434d5ad201bb9c3d25db4
Paul Deveaux <paul@pauldeveaux.com>
2021-07-03 11:44:47 -0700

    remove settings file from repo

--- stringstuff.py
+++ stringstuff.py
@@ -1,7 +1,36 @@
 def show_reverse_letters(string_in: str) -> str:
     n = len(string_in)
     reverse_string = ""
-    for i in range(n - 1, -1,-1):
+    for i in range(n - 1, -1, -1):
         reverse_string += string_in[i]
     return reverse_string
 
+
+def make_duckling_names():
+    prefixes = "JKLMNOPQ"
+    suffix = "ack"
+
+    for letter in prefixes:
+        if letter in "OQ":
+            print(letter + "u" + suffix)
+        else:
+            print(letter + suffix)
+
+
+def find(word: str, letter: str, index) -> int:
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index += 1
+    return -1
+
+
+def count(word: str, letter: str) -> int:
+    """
+    Returns the number of times letter appears in word
+    """
+    count = 0
+    for char in word:
+        if char == letter:
+            count += 1
+    return count
--- tests/test_stringstuff.py
+++ tests/test_stringstuff.py
@@ -1,5 +1,5 @@
 import pytest
-from stringstuff import show_reverse_letters
+from stringstuff import show_reverse_letters, find, count
 
 
 @pytest.mark.parametrize(
@@ -12,4 +12,30 @@ from stringstuff import show_reverse_letters
 )
 def test_show_reverse_letters(string_in, expected):
     actual = show_reverse_letters(string_in)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "word, letter, index, expected",
+    [
+        ("poster", "o", 0, 1),
+        ("superman", "p", 3, -1),
+        ("psuperman", "p", 0, 0),
+        ("psuperman", "p", 2, 3),
+    ],
+)
+def test_find(word, letter, index, expected):
+    actual = find(word, letter, index)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "word, letter, expected",
+    [
+        ("banana", "a", 3),
+        ("spiderman", "q", 0),
+    ],
+)
+def test_count(word, letter, expected):
+    actual = count(word, letter)
     assert actual == expected
